e113ba8b405e690785c0cc8ab99ac7fe6df7a85b
Gary Smith <garysmith193358@gmail.com>
2023-04-11 17:11:21 -0400

    completed flood fill

---
+++ Week1/FloodFill.js
@@ -0,0 +1,44 @@
+var floodFill = function(image, sr, sc, color) {
+  const startColor = image[sr][sc]
+  if ( startColor === color ) return image;
+
+  const visited = new Set();
+  return fill(image, sr, sc, color, visited, startColor)
+};
+
+
+const fill = (image, sr, sc, color, visited, startColor) => {
+  const rowInbound = 0 <= sr && sr < image.length;
+  const colInbound = 0 <= sc && sc < image[0].length;
+
+  const validPos = rowInbound && colInbound;
+  if (!validPos) return image;
+
+  console.log('hi', sr, sc, visited)
+
+  const pos = `${sr}:${sc}`
+
+  if (visited.has(pos)) return image;
+  visited.add(pos)
+
+  if ( image[sr][sc] !== startColor ) return image; 
+
+  //set it to new color 
+  image[sr][sc] = color;
+
+  //check directions;
+
+  //up
+  fill(image, sr, sc - 1, color, visited, startColor)
+
+  //down 
+   fill(image, sr, sc + 1, color, visited, startColor)
+
+  //left
+  fill(image, sr - 1, sc, color, visited, startColor)
+
+  //right
+  fill(image, sr + 1, sc, color, visited, startColor)
+
+  return image;
+} 
